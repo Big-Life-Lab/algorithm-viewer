df6bb46492d28d373b8cb6c2862b72c9e57b6cb5
martinwellman <mwellman@ohri.ca>
2026-01-23 16:47:28 -0700

    Added (commented-out) NULL value for model_definitions: In the future we will want to start up with no models loaded, rather than load the default HTNPoRT configuration.

--- global.R
+++ global.R
@@ -3,3 +3,5 @@ source("R/model_definitions/model_definitions.R")
 model_definitions <- read_model_definitions(
   file.path("data", "models", "htnport-mpp", "htnport-reduced.yaml")
 )
+
+# model_definitions <- NULL
