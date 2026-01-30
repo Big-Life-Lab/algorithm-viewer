e0f1d85e5f98dd1c7ea23f7042b43285a24116b0
martinwellman <mwellman@ohri.ca>
2026-01-30 09:40:22 -0700

    Added favicons, minor changes to look of UI, added model_message htmlOutput for general messages about the current model (eg. "no models loaded" message), added file upload control (for archived algorithm files), help panel

--- ui.R
+++ ui.R
@@ -4,30 +4,48 @@ library(shinyWidgets)
 library(bslib)
 library(plotly)
 
-plot_height <- "700px"
+# Standard height of the plots
+plot_height <- "calc(100vh - 170px)"
 
 ui <- fluidPage(
-  # fillPage(),
+  title = "Algorithm Viewer",
+  tags$head(
+    tags$link(rel = "icon", type = "image/png", sizes = "128x128", href = "/favicon-128x128.png"),
+    tags$link(rel = "icon", type = "image/png", sizes = "64x64", href = "/favicon-64x64.png"),
+    tags$link(rel = "icon", type = "image/png", sizes = "32x32", href = "/favicon-32x32.png"),
+    tags$link(rel = "icon", type = "image/png", sizes = "180x180", href = "/favicon-180x180.png")
+  ),
+  tags$style(HTML("
+    .shiny-input-container:not(.shiny-input-container-inline) {
+      width: unset;
+    }
+  ")),
+
   # Application title
   titlePanel(
     htmlOutput("ui_title")
   ),
 
+  # This style gets rid of the 300px width of the controls within the panels, to make them wider.
   # Sidebar layout
   sidebarLayout(
     # Sidebar panel for inputs
-    sidebarPanel(
+    mainPanel(
       width = 3,
-
       tabsetPanel(
         type = "tabs",
         tabPanel(
           "Models",
+          icon = icon("atom"),
           div(
             br(),
-
-            htmlOutput("ui_model_selection"),
-
+            htmlOutput("model_message"),
+            fileInput("upload", "Upload Algorithm:", accept = c(".zip", ".tar", ".gz")),
+            hr(),
+            checkboxGroupInput(
+              inputId = "model_id",
+              label = "Models:"
+            ),
             hr(),
 
             # Predictor selection (will be populated dynamically)
@@ -37,37 +55,27 @@ ui <- fluidPage(
               choices = NULL
             ),
 
+            # Interaction predictor selection (will be populated dynamically)
             selectInput(
               inputId = "interaction_predictor",
               label = "Interaction Predictor:",
               choices = NULL
             ),
-
             hr(),
 
             # Log or non-log scale
             checkboxInput("logarithmic", "Logarithmic", value = TRUE),
-
-            hr(),
-
-            fileInput("upload", "Upload Algorithm (.zip):", accept = ".zip")
           )
         ),
+        # Controls for the reference groups get added as children to #refgroups
         tabPanel(
           "Reference",
+          icon = icon("angle-double-down"),
           div(
-            htmlOutput("refgroups")
+            style = "height: calc(100vh - 150px); overflow-y: scroll",
+            div(id = "refgroups")
           )
         )
-      ),
-
-      hr(),
-
-      # Help button
-      actionButton(
-        inputId = "help",
-        label = "Help",
-        icon = icon("question-circle")
       )
     ),
 
@@ -80,15 +88,25 @@ ui <- fluidPage(
         type = "tabs",
         tabPanel(
           "Odds Ratio",
+          icon = icon("chart-line"),
           br(),
           plotlyOutput("or_plot", height = plot_height)
         ),
         tabPanel(
           "Predicted Risk",
+          icon = icon("chart-line"),
           br(),
           plotlyOutput("pr_plot", height = plot_height)
+        ),
+        tabPanel(
+          "Help",
+          icon = icon("circle-question"),
+          style = "max-width: 800px",
+          br(),
+          htmlOutput("help")
         )
-      )
+      ),
+      br()
     )
   )
 )
