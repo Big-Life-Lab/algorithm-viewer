335a079c635e405548fd4bbae68de9f730b498d5
martinwellman <mwellman@ohri.ca>
2026-06-15 11:21:21 -0700

    Move package titles and names to global variables, for easier changes to packages displayed by package_versions_ui function.

--- R/utils_meta.R
+++ R/utils_meta.R
@@ -1,3 +1,25 @@
+# List of packages for package_versions_ui function to always show
+# (whether algorithm_viewer_only is TRUE or FALSE). Fields are:
+#   \code{$title} descriptive title to show
+#   \code{$package} package name (eg. model.parameters.pipeline)
+.package_versions_ui_main_packages <- list(
+  list(
+    title = "Algorithm Viewer",
+    package = utils::packageName()
+  )
+)
+
+# List of packages for package_versions_ui function to show (in addition to
+# main_packages) only when algorithm_viewer_only is FALSE. Fields are:
+#   \code{$title} descriptive title to show
+#   \code{$package} package name (eg. model.parameters.pipeline)
+.package_versions_ui_other_packages <- list(
+  list(
+    title = "Model Parameters Pipeline",
+    package = "model.parameters.pipeline"
+  )
+)
+
 #' Package Versions UI
 #'
 #' Returns a \code{shiny::div} displaying the current version of the Algorithm
@@ -19,16 +41,30 @@ package_versions_ui <- function(
   algorithm_viewer_only = FALSE,
   color = "#999"
 ) {
+  # Gather HTML of packages and versions associated with the packages named
+  # list. Each element in the packages list is a named list with names
+  # $title (descriptive title to show for the package) and $package
+  # (R package to get the version of eg. "model.parameters.pipeline")
+  make_packages_html <- function(packages) {
+    v <- tagList()
+    for (info in packages) {
+      if (length(v) > 0) v[[length(v) + 1]] <- shiny::br()
+      v[[length(v) + 1]] <- paste0(
+        info$title,
+        " v",
+        utils::packageVersion(info$package)
+      )
+    }
+    v
+  }
+
   shiny::div(
     style = paste("color:", color),
-    paste0("Algorithm Viewer v", utils::packageVersion(utils::packageName())),
+    make_packages_html(.package_versions_ui_main_packages),
     if (!algorithm_viewer_only) {
       shiny::tagList(
         shiny::br(),
-        paste0(
-          "Model Parameters Pipeline v",
-          utils::packageVersion("model.parameters.pipeline")
-        )
+        make_packages_html(.package_versions_ui_other_packages)
       )
     }
   )
